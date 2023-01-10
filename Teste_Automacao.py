a29492183f857a1bb4f695b934a6ed888b28c7bc
Danilo Baptista <94879140+baptistadb@users.noreply.github.com>
2023-01-09 21:59:58 -0300

    Add files via upload

---
+++ Teste_Automacao.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[21]:
+
+
+import pyautogui as pg
+import time
+
+pg.hotkey('winleft')
+pg.write('bloco de notas')
+pg.hotkey('enter')
+time.sleep(2)
+pg.write('Testando automacao em pesquisas')
+time.sleep(3)
+pg.hotkey('alt', 'f4')
+pg.hotkey('tab')
+pg.hotkey('enter')
+time.sleep(2)
+pg.hotkey('winleft')
+pg.write('chrome')
+time.sleep(1)
+pg.hotkey('enter')
+time.sleep(2)
+pg.write('temperatura em junqueiropolis')
+pg.hotkey('enter')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+Testando automacao em pesquisas
+
