92aa2d03063d4726322a198df4e1f438989d4d1c
Danilo Baptista <94879140+baptistadb@users.noreply.github.com>
2023-01-10 11:08:52 -0300

    Add files via upload

---
+++ Automacao_Email.py
@@ -0,0 +1,196 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 36,
+   "id": "9ce962b2",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "import pyautogui as pg\n",
+    "import time\n",
+    "\n",
+    "pg.hotkey('winleft')\n",
+    "pg.write('bloco de notas')\n",
+    "time.sleep(2)\n",
+    "pg.hotkey('enter')\n",
+    "time.sleep(2)\n",
+    "pg.write('Teste de automacao em e-mail')\n",
+    "time.sleep(3)\n",
+    "pg.hotkey('alt', 'f4')\n",
+    "pg.hotkey('tab')\n",
+    "pg.hotkey('enter')\n",
+    "time.sleep(2)\n",
+    "pg.hotkey('winleft')\n",
+    "pg.write('chrome')\n",
+    "time.sleep(1)\n",
+    "pg.hotkey('enter')\n",
+    "time.sleep(2)\n",
+    "pg.write('gmail.com.br')\n",
+    "pg.hotkey('enter')\n",
+    "time.sleep(4)\n",
+    "pg.hotkey('tab')\n",
+    "pg.click(x=80, y=205, clicks=1)\n",
+    "time.sleep(1)\n",
+    "pg.click(x=1258, y=266, clicks=1)\n",
+    "time.sleep(1)\n",
+    "pg.write('danilobaptista12@outlook.com')\n",
+    "pg.hotkey('tab')\n",
+    "pg.hotkey('tab')\n",
+    "pg.write('Teste automacao')\n",
+    "pg.hotkey('tab')\n",
+    "pg.write('Este email foi gerado automaticamente\\n\\n')\n",
+    "time.sleep(1)\n",
+    "pg.write('Vamos anexar um arquivo')\n",
+    "time.sleep(2)\n",
+    "pg.click(x=324, y=672, clicks=1)\n",
+    "time.sleep(1)\n",
+    "pg.write('p')\n",
+    "pg.hotkey('down')\n",
+    "pg.hotkey('enter')\n",
+    "time.sleep(2)\n",
+    "pg.write('\\n\\nLEGAL! Agora e so enviar.')\n",
+    "time.sleep(2)\n",
+    "pg.hotkey('ctrl', 'enter')"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 34,
+   "id": "7645be83",
+   "metadata": {
+    "collapsed": true
+   },
+   "outputs": [],
+   "source": [
+    "import pyautogui as pg\n",
+    "import time\n",
+    "\n",
+    "pg.hotkey('winleft')\n",
+    "pg.write('chrome')\n",
+    "time.sleep(1)\n",
+    "pg.hotkey('enter')\n",
+    "time.sleep(2)\n",
+    "pg.write('gmail.com.br')\n",
+    "pg.hotkey('enter')\n",
+    "time.sleep(6)\n",
+    "pg.hotkey('tab')\n",
+    "pg.click(x=80, y=205, clicks=1)\n",
+    "time.sleep(1)\n",
+    "pg.click(x=1258, y=266, clicks=1)\n",
+    "time.sleep(1)\n",
+    "pg.write('danilobaptista12@outlook.com')\n",
+    "pg.hotkey('tab')\n",
+    "pg.hotkey('tab')\n",
+    "pg.write('Teste automacao')\n",
+    "pg.hotkey('tab')\n",
+    "pg.write('Este email foi gerado automaticamente\\n\\n')\n",
+    "time.sleep(1)\n",
+    "pg.write('Vamos anexar um arquivo')\n",
+    "time.sleep(2)\n",
+    "pg.click(x=324, y=672, clicks=1)\n",
+    "time.sleep(1)\n",
+    "pg.write('p')\n",
+    "pg.hotkey('down')\n",
+    "pg.hotkey('enter')\n",
+    "time.sleep(2)\n",
+    "pg.write('\\n\\nLEGAL! Agora e so enviar.')\n",
+    "time.sleep(2)\n",
+    "pg.hotkey('ctrl', 'enter')"
+   ]
+  },
+  {
+   "cell_type": "raw",
+   "id": "b369a11f",
+   "metadata": {},
+   "source": []
+  },
+  {
+   "cell_type": "markdown",
+   "id": "39f74ab8",
+   "metadata": {},
+   "source": []
+  },
+  {
+   "cell_type": "markdown",
+   "id": "74a1bb2c",
+   "metadata": {},
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "973adf57",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "9faf8f44",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 25,
+   "id": "d3b6af0b",
+   "metadata": {
+    "scrolled": true
+   },
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "Point(x=324, y=672)"
+      ]
+     },
+     "execution_count": 25,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "import pyautogui as pg\n",
+    "import time\n",
+    "\n",
+    "\n",
+    "time.sleep(4)\n",
+    "pg.position()"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "cdf7b9d1",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "\n"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.7"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
